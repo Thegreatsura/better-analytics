f3727b7b6bfadca78f038e21950292becc900275
Hyteq <DerpsDer.es@gmail.com>
2025-07-03 21:41:09 +0300

    feat: clickhouse package

---
+++ packages/db/src/client.ts
@@ -0,0 +1,13 @@
+import { drizzle } from 'drizzle-orm/node-postgres';
+import * as schema from './drizzle/schema';
+import * as relations from './drizzle/relations';
+
+const fullSchema = { ...schema, ...relations };
+
+const databaseUrl = process.env.DATABASE_URL as string;
+
+if (!databaseUrl) {
+    throw new Error('DATABASE_URL is not set');
+}
+
+export const db = drizzle(databaseUrl, { schema: fullSchema });
