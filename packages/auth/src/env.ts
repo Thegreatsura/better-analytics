fe294329219b5169618683050d8f89bca63a47e0
armfuldev <hello@armful.dev>
2025-07-04 17:48:10 +0200

    chore: env cleanups and formatting

--- packages/auth/src/env.ts
+++ packages/auth/src/env.ts
@@ -1,13 +1,17 @@
-import { createEnv } from "@better-analytics/env";
+import { createEnv, envValue } from "@better-analytics/env";
 import { z } from "zod";
 
-const envSchema = createEnv({
+export default createEnv({
 	schema: {
 		BETTER_AUTH_SECRET: z.string(),
 		BETTER_AUTH_URL: z.string(),
 		GITHUB_CLIENT_ID: z.string(),
 		GITHUB_CLIENT_SECRET: z.string(),
 	},
+	values: {
+		BETTER_AUTH_URL: envValue(
+			"http://localhost:3000",
+			"https://better-analytics.vercel.app",
+		),
+	},
 });
-
-export default envSchema;
--- packages/env/src/types.ts
+++ packages/env/src/types.ts
@@ -1,13 +1,13 @@
-import { z } from 'zod'
+import type { z } from "zod";
 
 export type Simplify<T> = {
-  [P in keyof T]: T[P]
-} & {}
+	[P in keyof T]: T[P];
+} & {};
 
 export type InferZodSchema<T> = {
-  [K in keyof T]: T[K] extends z.ZodType ? z.infer<T[K]> : never
-}
+	[K in keyof T]: T[K] extends z.ZodType ? z.infer<T[K]> : never;
+};
 
 export type SchemaValues<T extends Record<string, z.ZodType>> = {
-  [K in keyof T]?: z.infer<T[K]>
-}
+	[K in keyof T]?: z.infer<T[K]>;
+};
