2ed38d25bcb272b66a6b7ba5dd86678a7ec20dea
armfuldev <hello@armful.dev>
2025-07-04 13:52:02 +0200

    chore: cleanups

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,33 +1,7 @@
-import { auth } from "@better-analytics/auth";
-import { headers } from "next/headers";
-import { Suspense } from "react";
-import { SignButton } from "./_components/sign-button";
-
-export default async function HomePage() {
-	const session = await auth.api.getSession({
-		headers: await headers(),
-	});
-
+export default function Landing() {
 	return (
-		<div>
-			{session ? (
-				<div>
-					<p>Welcome {session.user.name}!</p>
-					{/* <form action={async () => {
-						"use server";
-						await auth.signOut();
-					}}>
-						<button type="submit">Sign Out</button>
-					</form> */}
-				</div>
-			) : (
-				<div>
-					<p>You are not logged in</p>
-					<Suspense fallback={<div className="h-10" />}>
-						<SignButton session={session} />
-					</Suspense>
-				</div>
-			)}
+		<div className="container mx-auto flex h-svh w-full flex-col items-center justify-center">
+			<h2 className="font-medium">Wow, hackathon!</h2>
 		</div>
 	);
 }
