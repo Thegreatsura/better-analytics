cbc32ed64d54378360049e597f861be6a9d329dc
Hyteq <DerpsDer.es@gmail.com>
2025-07-04 14:43:35 +0300

    meow

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -1,11 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import * as schema from './drizzle/schema';
+import * as analyticsSchema from './drizzle/analytics.schema';
+import * as userSchema from './drizzle/schema';
 
-// Create a PostgreSQL pool
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL!,
 });
 
-// Create the Drizzle client
-export const db = drizzle(pool, { schema: schema });
+export const db = drizzle(pool, {
+    schema: { ...analyticsSchema, ...userSchema },
+});
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,2 +1,4 @@
 export * from './client';
 export * from './clickhouse/client';
+export * from './drizzle/analytics.schema';
+export * from './drizzle/schema';
