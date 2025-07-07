c08db8c6f1411bb55a657b9dccde066512057601
Hyteq <DerpsDer.es@gmail.com>
2025-07-07 13:26:21 +0300

    bonkers

--- apps/demo/src/env.ts
+++ apps/demo/src/env.ts
@@ -2,16 +2,16 @@ import { createEnv } from "@better-analytics/env";
 import { z } from "zod";
 
 const envSchema = {
-	API_URL: z.string(),
-	CLIENT_ID: z.string(),
-	ACCESS_TOKEN: z.string(),
+	NEXT_PUBLIC_API_URL: z.string(),
+	NEXT_PUBLIC_CLIENT_ID: z.string(),
+	NEXT_PUBLIC_ACCESS_TOKEN: z.string(),
 	NODE_ENV: z.enum(["development", "production"]),
 };
 
 export default createEnv({
 	schema: envSchema,
 	values: {
-		API_URL: "http://localhost:4000",
+		NEXT_PUBLIC_API_URL: "http://localhost:4000",
 		NODE_ENV: "development",
 	},
 });
--- apps/demo/src/lib/analytics.ts
+++ apps/demo/src/lib/analytics.ts
@@ -1,15 +1,9 @@
 /** biome-ignore-all lint/style/noNonNullAssertion: demo */
-import { init, initLogger } from "@better-analytics/sdk";
+import { init } from "@better-analytics/sdk";
+import env from "@/env";
 
 export const analytics = init({
-	apiUrl: process.env.API_URL!,
-	clientId: process.env.CLIENT_ID!,
-	accessToken: process.env.ACCESS_TOKEN!,
-});
-
-export const logger = initLogger({
-	apiUrl: process.env.API_URL!,
-	clientId: process.env.CLIENT_ID!,
-	accessToken: process.env.ACCESS_TOKEN!,
-	serviceName: "better-analytics-demo",
+	apiUrl: process.env.NEXT_PUBLIC_API_URL!,
+	clientId: process.env.NEXT_PUBLIC_CLIENT_ID!,
+	accessToken: process.env.NEXT_PUBLIC_ACCESS_TOKEN!,
 });
