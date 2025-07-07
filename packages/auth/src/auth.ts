0b7888d2caf2a7564d6e5961b2795221a349e27a
armfuldev <hello@armful.dev>
2025-07-07 14:46:48 +0200

    feat(auth): welcome emails

--- packages/auth/src/auth.ts
+++ packages/auth/src/auth.ts
@@ -2,8 +2,11 @@ import "dotenv/config";
 
 import { betterAuth, generateId } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
 import { db } from "@better-analytics/db";
 import env from "@better-analytics/auth/env";
+import { resend } from "@better-analytics/email";
+import  WelcomeEmail from "@better-analytics/email/welcome";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -15,6 +18,20 @@ export const auth = betterAuth({
 		github: {
 			clientId: env.GITHUB_CLIENT_ID,
 			clientSecret: env.GITHUB_CLIENT_SECRET,
+		},
+	},
+	databaseHooks: {
+		user: {
+			create: {
+				after: async (user) => {
+					await resend.emails.send({
+						from: "Better Analytics <noreply@kly.ng>",
+						to: user.email,
+						subject: "Welcome to Better Analytics",
+						react: WelcomeEmail({ username: user.name, url: env.BETTER_AUTH_URL }),
+					});
+				},
+			},
 		},
 	},
 });
